f64142748f529cff61f74666eb30da802980e83e
xuyjy036 <xuyjy036@mymail.unisa.edu.au>
2018-08-30 14:59:26 +0930

    Testing

---
+++ Android-DrugEducationCentre/PyServer/DrugEducationCentreServer/DrugEducationCentreServer/urls.py
@@ -0,0 +1,27 @@
+"""DrugEducationCentreServer URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from . import view
+
+urlpatterns = [
+    url(r'^$', view.login, name='login'),
+    url(r'^login/$', view.login, name='login'),
+    url(r'^regist/$', view.regist, name='regist'),
+    url(r'^index/$', view.index, name='index'),
+    url(r'^logout/$', view.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ Android-DrugEducationCentre/PyServer/README.md
@@ -0,0 +1,14 @@
+# Introduction 
+- based in Django
+- based in Python 2.x
+
+# Dependencies 
+- pip install Django
+
+
+
+
+# Admin (http://127.0.0.1:8000/admin)
+- Username: zhury007
+- Email address: zrl108@163.com
+- Password: qq123456
