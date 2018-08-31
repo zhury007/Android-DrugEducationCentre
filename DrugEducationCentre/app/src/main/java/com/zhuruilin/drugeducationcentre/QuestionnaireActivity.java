8055cf6dbb884b14d35afb7ac32f921f4465288b
zhury007 <zrl108@163.com>
2018-08-31 22:06:38 +0800

    Complete the function of the questionnaire
    Not test

--- DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/QuestionnaireActivity.java
+++ DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/QuestionnaireActivity.java
@@ -1,13 +1,56 @@
 package com.zhuruilin.drugeducationcentre;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class QuestionnaireActivity extends AppCompatActivity {
+    WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        webview = (WebView) findViewById(R.id.webView_questionnaire);
+        webview.loadUrl("https://wj.qq.com/s/2342832/f761");
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                } else if (newProgress == 100) {
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onDestroy() {
+        if (webview != null) {
+            webview.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webview.clearHistory();
+            ((ViewGroup) webview.getParent()).removeView(webview);
+            webview.destroy();
+            webview = null;
+        }
+        super.onDestroy();
+    }
+
+    
 }
