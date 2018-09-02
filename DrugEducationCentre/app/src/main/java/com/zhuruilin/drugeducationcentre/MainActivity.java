c61dd40fc7ad45ca0b73000cf1b98df7abeb9665
zhury007 <zrl108@163.com>
2018-09-02 10:07:01 +0800

    remove R.menu.main

--- DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/MainActivity.java
+++ DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+//        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -142,7 +142,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_setting) {
 
         } else if (id == R.id.nav_share) {
 
