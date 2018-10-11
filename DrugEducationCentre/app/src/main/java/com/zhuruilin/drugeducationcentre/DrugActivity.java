aeaad8f9d206f81abf7b7db8027efb2233fb60b8
JiaHua XU <31717336+jiahua310@users.noreply.github.com>
2018-10-12 04:28:44 +1030

    Add files via upload

---
+++ DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/DrugActivity.java
@@ -0,0 +1,30 @@
+package com.zhuruilin.drugeducationcentre;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.zhuruilin.drugeducationcentre.Drugs.Ecstasy.DrugFragment;
+
+public class DrugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drug);
+
+
+        // get the ecstasy activity
+        Button btn_ecstasy = (Button) findViewById(R.id.button_ecstasy);
+        btn_ecstasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrugActivity.this, DrugFragment.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
