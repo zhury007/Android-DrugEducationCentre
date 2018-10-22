a31e567c21b4a413481fe9f95e4367c0e57bcb41
JiaHua XU <31717336+jiahua310@users.noreply.github.com>
2018-10-23 04:53:08 +1030

    update Drug Activity file

--- DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/DrugActivity.java
+++ DrugEducationCentre/app/src/main/java/com/zhuruilin/drugeducationcentre/DrugActivity.java
@@ -7,24 +7,36 @@ import android.view.View;
 import android.widget.Button;
 
 import com.zhuruilin.drugeducationcentre.Drugs.Ecstasy.DrugFragment;
+import com.zhuruilin.drugeducationcentre.Drugs.Methamphetamine.MethamphetamineFragment;
 
 public class DrugActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drug_fragment);
+        setContentView(R.layout.activity_drug);
 
 
         // get the ecstasy activity
-//        Button btn_ecstasy = (Button) findViewById(R.id.button_ecstasy);
-//        btn_ecstasy.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view){
-//                Intent intent = new Intent(DrugActivity.this, DrugFragment.class);
-//                startActivity(intent);
-//            }
-//
-//        });
+        Button btn_ecstasy = (Button) findViewById(R.id.button_ecstasy);
+        btn_ecstasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrugActivity.this, DrugFragment.class);
+                startActivity(intent);
+            }
+
+        });
+
+        //get the Methamphetamine activity
+        Button btn_meth = (Button) findViewById(R.id.button_Methamphetamine);
+        btn_meth.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrugActivity.this, MethamphetamineFragment.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
