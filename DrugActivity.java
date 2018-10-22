edc012d1f9fbb243ed9c5b3d3c102e343645739b
JiaHua XU <31717336+jiahua310@users.noreply.github.com>
2018-10-23 04:51:13 +1030

    update

---
+++ DrugActivity.java
@@ -0,0 +1,41 @@
+package com.zhuruilin.drugeducationcentre;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.zhuruilin.drugeducationcentre.Drugs.Ecstasy.DrugFragment;
+import com.zhuruilin.drugeducationcentre.Drugs.Methamphetamine.MethamphetamineFragment;
+
+public class DrugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drug);
+
+
+        // get the ecstasy activity
+        Button btn_ecstasy = (Button) findViewById(R.id.button_ecstasy);
+        btn_ecstasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrugActivity.this, DrugFragment.class);
+                startActivity(intent);
+            }
+
+        });
+
+        //get the Methamphetamine activity
+        Button btn_meth = (Button) findViewById(R.id.button_Methamphetamine);
+        btn_meth.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(DrugActivity.this, MethamphetamineFragment.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
